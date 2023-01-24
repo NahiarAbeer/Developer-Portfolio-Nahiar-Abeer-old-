9b6ec0ad790049b385d188c48eb39976b532676b
NahiarAbeer <nahiarabir@gmail.com>
2023-01-24 18:42:23 +0600

    try 1

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-// Get the div
-var fadeInDiv = document.querySelector(".fade-in");
+// // Get the div
+// var fadeInDiv = document.querySelector(".fade-in");
 
-// Get the offset position of the div
-var fadeInDivPosition = fadeInDiv.getBoundingClientRect().top;
+// // Get the offset position of the div
+// var fadeInDivPosition = fadeInDiv.getBoundingClientRect().top;
 
-// Add the class to the div when the user scrolls down and the div is in view
-window.addEventListener("scroll", function () {
-    if (window.pageYOffset > fadeInDivPosition) {
-        fadeInDiv.classList.add("visible");
-    }
-});
+// // Add the class to the div when the user scrolls down and the div is in view
+// window.addEventListener("scroll", function () {
+//     if (window.pageYOffset > fadeInDivPosition) {
+//         fadeInDiv.classList.add("visible");
+//     }
+// });
+alert("Welcome to My Website")
